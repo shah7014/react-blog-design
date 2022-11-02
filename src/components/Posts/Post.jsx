270c744e70cbc519f278a38d8bc3265bb2309445
Romil Shah <romilshah2199@gmail.com>
2022-11-02 14:55:24 +0530

    feature: styled HomePage

---
+++ src/components/Posts/Post.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import classes from "./Post.module.css";
+
+const Post = (props) => {
+  return (
+    <div className={classes.post}>
+      <img src={props.img} alt="" className={classes.img} />
+
+      <div className="postCats">
+        <span className={classes.postCat}>Music</span>
+        <span className={classes.postCat}>Life</span>
+      </div>
+
+      <span className={classes.title}>Lorem ipsum dolor sit amet</span>
+
+      <span className={classes.time}>1 hour ago</span>
+
+      <p className={classes.content}>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque ea,
+        officiis consectetur beatae culpa nobis eveniet delectus, doloremque
+        vero magni voluptate nesciunt cupiditate. Odio tempora dicta distinctio
+        libero exercitationem cum.
+      </p>
+    </div>
+  );
+};
+
+export default Post;
--- src/components/Posts/PostsList.jsx
+++ src/components/Posts/PostsList.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import Post from "./Post";
 import classes from "./PostsList.module.css";
 
+const images = [
+  "https://images.unsplash.com/photo-1418985991508-e47386d96a71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1633117678642-7fcc7870f64a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80",
+  "https://images.unsplash.com/photo-1550147760-44c9966d6bc7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+  "https://images.unsplash.com/photo-1595351298020-038700609878?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  "https://images.unsplash.com/photo-1594882471743-2758d2ce5f00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+];
+
 const PostsList = () => {
-  return <div className={classes.posts}>Posts</div>;
+  return (
+    <div className={classes.posts}>
+      {images.map((img, index) => (
+        <Post key={index} img={img} />
+      ))}
+    </div>
+  );
 };
 
 export default PostsList;
